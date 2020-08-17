b1ad1482a3e8556cda8715e897648afee797ab14
HARI PRASAD <esshariprasad@gmail.com>
2020-08-17 19:15:22 +0530

    first api call using axios

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+async function getResults(query) {
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+    console.log(res);
+}
+
+getResults('pizza');
+
+
+
