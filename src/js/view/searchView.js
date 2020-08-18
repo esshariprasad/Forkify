0be16b1d51e4ac2552a699843f72d3dfd502bb33
HARI PRASAD <esshariprasad@gmail.com>
2020-08-18 15:49:22 +0530

    Building Search view part 2

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -13,6 +13,31 @@ export const clearResults = () =>{
     
 };
 
+
+const limitRecipeTitle = (title,limit = 17) => {
+    // 'pasta with tamato and spinach'
+    if(title.length > limit)
+        {
+            const newTitle = [];
+            title.split(' ').reduce((acc,curr)=>{
+                
+                if(acc + curr.length <= limit){
+                    newTitle.push(curr);
+                    
+                }
+                
+                return acc + curr.length;
+
+            },0) 
+            // /Reduce method: 1) callback function with acc and curr
+
+            // 2)initial value of the accumulator/
+        
+            return `${newTitle.join(' ')}...`; 
+            
+        }
+        return title;
+}
 const renderRecipe = recipe => {
     
     const markup = `
@@ -22,7 +47,7 @@ const renderRecipe = recipe => {
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
